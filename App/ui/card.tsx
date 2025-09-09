44305f857596639202322c22416189b1aa80ad53
Vishakha <vishakhashadija225@gmail.com>
2025-09-10 02:50:15 +0530

    Fixed libraries

--- App/ui/card.tsx
+++ App/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "UI/utils";
+import { cn } from "../ui/utils";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
